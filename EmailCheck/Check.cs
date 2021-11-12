1d73191334c231bfe8510867937246666d41ff36
erfan66 <erfan.azizifard@gmail.com>
2021-11-13 00:37:33 +0330

    Frist release of 'EmailCheck'.

---
+++ EmailCheck/Check.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailCheck
+{
+    public class Check : IComparable<Check>
+    {
+
+        public int Score { get;private set; }
+        public string Content { get;private set; }
+        public string FileName { get; set; }
+        public List<string> CommonFishingWords { get;private set; }
+        public Check()
+        {
+
+        }
+        public Check(string filename,string content,List<string> commonPhishingWords)
+        {
+            this.Content = content;
+            this.CommonFishingWords = commonPhishingWords;
+            this.FileName = filename;
+            CalcScore();
+        }
+        public void CalcScore()
+        {
+            foreach (var commWord in CommonFishingWords)
+            {
+                if (Content.Contains(commWord.ToLower()))
+                {
+                    Score++;
+                }
+            }
+            
+        }
+
+        public int CompareTo(Check other)
+        {
+            return this.Score.CompareTo(other.Score);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} --> {1}",FileName,Score);
+        }
+    }
+}
---
+++ EmailCheck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EmailCheck
+{
+    public class Program
+    {
+        Program()
+        {
+            var commonWord = new List<string>(File.ReadAllLines(@"../../../PhishingCommonWords.txt"));
+
+            List<Check> checks = new List<Check>();
+            foreach (var f in Directory.GetFiles(@"D:\VS2019\EmailCheck\EmailCheck\EmailCheck\Sample"))
+            {
+                
+                var check = new Check(f,File.ReadAllText(f), commonWord);
+                checks.Add(check);
+            }
+
+            checks.Sort();
+            checks.Reverse();
+            checks.ForEach(Console.WriteLine);
+
+            
+        }
+        static void Main(string[] args)
+        {
+            new Program();
+            
+        }
+    }
+}
